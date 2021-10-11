dd560770e44f663ae5ea0d7d18216a3c63ff2d9e
terrence <weichiantan09@gmail.com>
2021-10-12 05:03:52 +0800

    just recall merge sort and revamped the implementation

--- merge_sort/merge_sort.cpp
+++ merge_sort/merge_sort.cpp
@@ -4,34 +4,43 @@
 using namespace std;
 
 vector<int> simple_merge_sort(vector<int> x){
-    if(x.size()==1) return x;
+    if(x.size()<=1) return x;
     vector<int> sorted_list;
-    int midpoint=-1,mid_value;
+    int midpoint=-1;
     if(x.size()%2==0){
         midpoint=x.size()/2;
-        mid_value=x[midpoint];
     }else{
         midpoint=(x.size()-1)/2;
-        mid_value=x[midpoint+1];
     }
-    vector<int> left_x;
-    vector<int> right_x;
-    for(int i=0;i<x.size();i++){
-        if(x[i]<=mid_value){
-            left_x.push_back(x[i]);
-        }else right_x.push_back(x[i]);
-    }
+    vector<int> left_x={x.begin(),x.begin()+midpoint};
+    vector<int> right_x={x.begin()+midpoint,x.end()};
     vector<int> left_sorted=simple_merge_sort(left_x);
     vector<int> right_sorted=simple_merge_sort(right_x);
-    sorted_list=left_sorted;
-    sorted_list.insert(sorted_list.end(),right_sorted.begin(),right_sorted.end());
+    //merge phase
+    while(left_sorted.size()>0&&right_sorted.size()>0){
+        if(left_sorted[0]<right_sorted[0]){
+            sorted_list.push_back(left_sorted[0]);
+            left_sorted.erase(left_sorted.begin());
+        }else{
+            sorted_list.push_back(right_sorted[0]);
+            right_sorted.erase(right_sorted.begin());
+        }
+    }
+    while(left_sorted.size()>0){
+        sorted_list.push_back(left_sorted[0]);
+        left_sorted.erase(left_sorted.begin());
+    }
+    while(right_sorted.size()>0){
+        sorted_list.push_back(right_sorted[0]);
+        right_sorted.erase(right_sorted.begin());
+    }
     return sorted_list;
 }
 
 int main(int argc,char* argv[]){
     //generate a list for sorting
     vector<int> x;
-    for(int i=100;i>-100;i--) x.push_back(i);
+    for(int i=10;i>-10;i--) x.push_back(i);
     cout<<"original list:\n";
     for(int i=0;i<x.size();i++) cout<<x[i]<<" ";
     cout<<"\n";
